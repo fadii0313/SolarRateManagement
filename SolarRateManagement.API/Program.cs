4c7faded009b33ec3ca48cc3f17af50e8f4417d7
PC <PC@DESKTOP-FADII>
2026-09-01 19:05:45 +0500

    pdate target framework to net8.0 for Render Docker deployment

--- SolarRateManagement.API/Program.cs
+++ SolarRateManagement.API/Program.cs
@@ -74,18 +74,10 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<SolarRateManagement.API.Middleware.GlobalExceptionMiddleware>();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
